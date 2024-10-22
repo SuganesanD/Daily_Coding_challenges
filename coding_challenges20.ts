86829e4c02672e392c8d4431e501f5d97a23454f
SuganesanD <suganesan1808@gmail.com>
2024-10-22 18:02:48 +0530

    updated

--- coding_challenges20.ts
+++ coding_challenges20.ts
@@ -4,21 +4,15 @@ var age=input("enter the age:").toLowerCase();
 if(gender=="male"){
     if(Number(age)>=20 && Number(age)<40){
         console.log("anywhere");
-        
-
     }
     else if(Number(age)>=40 && Number(age)<=60){
-        console.log("urban");
-        
+        console.log("urban");  
     }
     else{
-        console.log("error");
-        
+        console.log("error");   
     }
-    
 }
 if(gender=="female"){
-    console.log("urban");
-    
+    console.log("urban"); 
 }
 
